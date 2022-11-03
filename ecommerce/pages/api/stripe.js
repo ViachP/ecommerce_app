9812d102374d8de4fb4f58a6c9ad5bad505ace8e
Slava <viachaslau.pailak@gmail.com>
2022-11-03 23:58:26 +0300

    implement stripe payment

---
+++ ecommerce/pages/api/stripe.js
@@ -0,0 +1,51 @@
+import Stripe from 'stripe';
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const params = {
+        submit_type: 'pay',
+        mode: 'payment',
+        payment_method_types: ['card'],
+        billing_address_collection: 'auto',
+        shipping_options: [
+            {shipping_rate: 'shr_1M09reIJxYaMkKufQGP4UXCR'},
+            {shipping_rate: 'shr_1M09u1IJxYaMkKuf4xmRO5nl'},
+        ],
+
+        line_items: req.body.cartItems.map((item) => {
+            const img = item.image[0].asset._ref;
+            const newImage = img.replace('image-', 'https://cdn.sanity.io/images/5d8363vr/production/').replace('-webp', '.webp');
+
+            return {
+                price_data: {
+                    currency: 'usd',
+                    product_data: {
+                        name: item.name,
+                        images: [newImage],
+                    },
+                    unit_amount: item.price * 100,
+                },
+                adjustable_quantity: {
+                    enabled: true,
+                    minimum: 1,
+                },
+                quantity: item.quantity
+            }
+        }),
+        
+        mode: 'payment',
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      };
+      res.redirect(303, session.url);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
