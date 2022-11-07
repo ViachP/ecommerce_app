5fb1ebe220f2348ff070ea0076de288fa4b52955
Slava <viachaslau.pailak@gmail.com>
2022-11-07 17:22:36 +0300

    implement confetti effect at success page

--- ecommerce/pages/success.js
+++ ecommerce/pages/success.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { BsBagCheckFill } from 'react-icons/bs'
 import { useStateContext } from '../context/StateContext'
+import { runFireworks } from '../lib/utils';
 
 const Success = () => {
     const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
@@ -13,7 +14,7 @@ const Success = () => {
       setTotalQuantities(0);
       runFireworks();
     }, []);
-    
+
   return (
     <div className='success-wrapper'>
       <div className='success'>
