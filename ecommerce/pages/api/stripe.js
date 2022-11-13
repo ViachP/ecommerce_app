86029a089cc538dff85f5c2ea15f8415a9e1cbfc
Slava <viachaslau.pailak@gmail.com>
2022-11-13 23:36:34 +0300

    fix some issues wirh stripe

--- ecommerce/pages/api/stripe.js
+++ ecommerce/pages/api/stripe.js
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
@@ -12,10 +12,9 @@ export default async function handler(req, res) {
         billing_address_collection: 'auto',
         shipping_options: [
             {shipping_rate: 'shr_1M09reIJxYaMkKufQGP4UXCR'},
-            {shipping_rate: 'shr_1M09u1IJxYaMkKuf4xmRO5nl'},
         ],
 
-        line_items: req.body.cartItems.map((item) => {
+        line_items: req.body.map((item) => {
             const img = item.image[0].asset._ref;
             const newImage = img.replace('image-', 'https://cdn.sanity.io/images/5d8363vr/production/').replace('-webp', '.webp');
 
@@ -36,11 +35,14 @@ export default async function handler(req, res) {
             }
         }),
         
-        mode: 'payment',
-        success_url: `${req.headers.origin}/success=true`,
-        cancel_url: `${req.headers.origin}/canceled=true`,
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
       };
-      res.redirect(303, session.url);
+
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+
+      res.status(200).json(session);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
