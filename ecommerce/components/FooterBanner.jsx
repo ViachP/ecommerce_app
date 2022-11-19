e2beb4b34741fac98ee6601cc29b3d46263174fe
Slava <viachaslau.pailak@gmail.com>
2022-11-19 14:20:50 +0300

    change styles for mobile footer banner

--- ecommerce/components/FooterBanner.jsx
+++ ecommerce/components/FooterBanner.jsx
@@ -7,16 +7,16 @@ const FooterBanner = ({ footerBanner : {discount, largeText1, largeText2, saleTi
   return (
     <div className="footer-banner-container">
       <div className="banner-desc">
-        <div className="left">
+        {/* <div className="left">
           <p>{discount}</p>
           <h3>{largeText1}</h3>
           <h3>{largeText2}</h3>
           <p>{saleTime}</p>
-        </div>
+        </div> */}
         <div className="right">
           <p>{smallText}</p>
           <h3>{midText}</h3>
-          <p>{desc}</p>
+          <p>{discount}</p>
           <Link href={`/product/${product}`}>
             <button type="button">{buttonText}</button>
           </Link>
